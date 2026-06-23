6f60a9dd5067c7e9fb648cd6e75e5f858846c058
IT26100209 <IT26100209@my.sliit.lk>
2026-06-23 21:33:14 +0530

    answer lab2Q2

---
+++ Q2/Lab2Q2.java
@@ -0,0 +1,10 @@
+public class Lab2Q2
+{
+  public static void main(String[]args)
+  {
+  double side=10;
+  double ropeLength=4*side;
+  double radius=ropeLength/(2*Math.PI);
+  System.out.println("Radius of the circular fence = "+radius);
+  }
+}
